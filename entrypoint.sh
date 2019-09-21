38980c575a4410aeab4e7c2d5e63d03eb8256986
Gilbert Brault <gilbert.brault@hotmail.com>
2019-09-21 16:33:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rsyslogd -n &
+
 # When not limiting the open file descritors limit, the memory consumption of
 # slapd is absurdly high. See https://github.com/docker/docker/issues/8231
 ulimit -n 8192
